7b35fd9a613b5fc22a4f37a9ac829dc7744ba866
9128hotel <9128hotel@gmail.com>
2023-04-06 22:43:43 +0200

    Moved check.c to its own MSVS project
    	modified:   .gitignore
    	new file:   check/check.sln
    	renamed:    src/check.c -> check/check/check.c
    	new file:   check/check/check.vcxproj
    	new file:   check/check/check.vcxproj.filters
    	new file:   check/check/check.vcxproj.user
    	modified:   executables/check.exe
    	new file:   executables/check.pdb

--- check/check/check.c
+++ check/check/check.c
@@ -1,11 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS // i do not care
+
 #include <stdio.h>
 #include <stdlib.h>
 
 // Include the appropriate header for checking access rights based on the platform
-#ifdef _WIN32
-#include <io.h>
+#ifdef _WIN32 // checks if program is running on windows
+#include <io.h> // io.h for windows only
 #else
-#include <unistd.h>
+#include <unistd.h> // unistd.h only works on linux/unix systems
 #endif
 
 int main() {
@@ -16,7 +18,7 @@ int main() {
     // create file in "path" then immideately delete it
     FILE* temp_file = tmpfile(); // create file
     if (temp_file) { // if file was created successfully
-        char* temp_file_path = mktemp(temp_file_name); // create a path to the file
+        char* temp_file_path = _mktemp(temp_file_name); // create a path to the file
         if (temp_file_path) { // if the file was created successfully
             fclose(temp_file); // close the temp file
             remove(temp_file_path); // delete the temp file
@@ -27,7 +29,8 @@ int main() {
     // print the result
     if (result == 0) {
         printf("%s is writable\n", path);
-    } else {
+    }
+    else {
         printf("%s is not writable\n", path);
     }
 
